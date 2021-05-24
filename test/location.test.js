302ed03543c369a0d3fa8749fd5ee29b29ee1f60
lican02 <lican02@daojia-inc.com>
2021-05-24 13:45:10 +0800

    delete: ak

--- test/location.test.js
+++ test/location.test.js
@@ -2,22 +2,22 @@
  * @author: lican02
  * @description:
  */
-describe('location API:', function () {
-  describe('#locateByBaidu()', function () {
-    before(function () {
-      // window.location.protocol = "https"
-    })
-    it(`locateByBaidu(), 百度 ip 定位`, function (done) {
-      utils.locateByBaidu("dvrEqPombaLKTVpKCSIj9pyF").then(res => {
-        console.log(res)
-        assert(res.lng && res.lng > 0)
-        done()
-      })
-        .catch(err => {
-          done(err)
-        })
+// describe('location API:', function () {
+//   describe('#locateByBaidu()', function () {
+//     before(function () {
+//       // window.location.protocol = "https"
+//     })
+//     it(`locateByBaidu(), 百度 ip 定位`, function (done) {
+//       utils.locateByBaidu("").then(res => {
+//         console.log(res)
+//         assert(res.lng && res.lng > 0)
+//         done()
+//       })
+//         .catch(err => {
+//           done(err)
+//         })
 
-    });
-  });
+//     });
+//   });
 
-});
+// });
